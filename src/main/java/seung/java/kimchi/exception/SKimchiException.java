30f4e7a7f6e95019ea5344fc1b0f555d85432568
stoas <stoas@118.37.196.226>
2020-06-07 05:21:49 +0900

    divide SString into SString and SConvert

--- src/main/java/seung/java/kimchi/exception/SKimchiException.java
+++ src/main/java/seung/java/kimchi/exception/SKimchiException.java
@@ -4,12 +4,12 @@ public class SKimchiException extends Exception {
 
     private static final long serialVersionUID = 5500146280392382953L;
     
-    public SKimchiException(String message) {
-        super(message);
-    }
-    
     public SKimchiException(Throwable e) {
         super(e);
+    }
+    
+    public SKimchiException(String message) {
+        super(message);
     }
     
     public SKimchiException(String message, Throwable e) {
