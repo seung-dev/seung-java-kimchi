dafbcea0d1e16af9ece6a15c442eb136522a4d37
seung <seung@175.210.94.30>
2020-06-23 04:10:19 +0900

    add SCode

--- src/main/java/seung/java/kimchi/util/SCode.java
+++ src/main/java/seung/java/kimchi/util/SCode.java
@@ -1,7 +1,11 @@
 package seung.java.kimchi.util;
 
-public class SResponseE {
+public class SCode {
 
+	public final static String START = "((START))";
+	
+	public final static String END = "((END))";
+	
 	public final static String SUCCESS = "0000";
 	
 	public final static String ERROR = "E999";
